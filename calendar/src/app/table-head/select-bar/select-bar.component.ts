60c8d1081fdeb83f52fc86e034a43dd9a564c0fd
llladno <man30968@gmail.com>
2023-11-25 09:56:55 +0300

    add index html

--- calendar/src/app/table-head/select-bar/select-bar.component.ts
+++ calendar/src/app/table-head/select-bar/select-bar.component.ts
@@ -1,11 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-select-bar',
   templateUrl: './select-bar.component.html',
   styleUrls: ['./select-bar.component.css']
 })
-export class SelectBarComponent {
+export class SelectBarComponent implements OnInit {
  @Input() data:any;
  @Input() nextMonth: any;
+
+  constructor(private route:ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    console.log(this.data)
+  }
+
 }
--- calendar/src/app/table-head/table-head.component.ts
+++ calendar/src/app/table-head/table-head.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {delay, Observable} from "rxjs";
+
 
 @Component({
   selector: 'app-table-head',
@@ -13,22 +15,86 @@ export class TableHeadComponent implements OnInit{
 
 
   loading: boolean = false;
-  date: any;
   today: Date;
+  isDay: boolean = false;
+  dayInMonth: any
 
-  constructor(private route:ActivatedRoute) {
+  constructor(private route:ActivatedRoute,
+  private router: Router) {
   }
 
   ngOnInit() {
     this.today = new Date()
+    if (this.data.day) this.isDay = true
+    else if(!this.data.day) this.isDay = false
+
+
     console.log(this.data)
   }
   nextMonth(){
+
+    console.log(this.data)
     this.loading = true
     this.data.month = this.route.snapshot.queryParams.month
+    if (this.data.day) this.isDay = true
+    else if(!this.data.day) this.isDay = false
     this.parentValueChange.emit(this.data)
+    this.route.queryParams.subscribe((x:any)=>{
+      window.location.reload()
+    })
       setTimeout(()=>{
         window.location.reload()
       },20)
+    console.log(this.dayInMonth)
+  }
+
+  nextDay(direction:string){
+    let newData:any = {}
+    function getDaysInMonth(year:number, month:number) {
+      return new Date(year, month, 0).getDate();
+    }
+    this.dayInMonth = getDaysInMonth(+this.data.year,+this.data.month)
+    if (direction === 'next'){
+      newData = {
+        day: +this.data.day > +this.dayInMonth-1 ? 1: +this.data.day+1,
+        month: +this.data.month > 11 ? 1 : +this.data.month,
+        year:+this.data.month > 11 ? +this.data.year+1 :this.data.year
+      }
+      console.log(newData)
+      if (newData.day - +this.data.day < 0) {
+        console.log('changed')
+        newData.month = +this.data.month+1 > 12 ? 1 : +this.data.month+1
+      }
+    }
+
+
+
+  else {
+        newData = {
+          day: +this.data.day > +this.dayInMonth-1 ? 1: +this.data.day-1,
+          month: +this.data.month < 1 ? 1 : +this.data.month,
+          year:+this.data.month < 0 ? +this.data.year-1 :this.data.year
+        }
+      console.log(newData)
+        if (newData.day  < 1) {
+          console.log('changed')
+          newData.day = getDaysInMonth(this.data.year, this.data.month-1)
+          newData.month = +this.data.month-1 < 0 ? 1 : +this.data.month-1
+        }
+      console.log(newData)
+    }
+      // const urlTree = this.router.createUrlTree([], {
+      //   relativeTo: this.route,
+      //   queryParams: {'day': newData.day,'month': newData.month, 'year': newData.year},
+      //   queryParamsHandling: "merge",
+      //   preserveFragment: true });
+      // this.router.navigateByUrl(urlTree)
+      //   .then(()=>window.location.reload());
+
+}
+  changeDay(){
+    this.loading = true
+    this.route.queryParams.pipe(delay(10)).subscribe(()=> window.location.reload())
+
   }
 }
